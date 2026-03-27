dcbd5032e3215effc8a663e548c549e4ae02c297
systemaditya <67898580+systemaditya@users.noreply.github.com>
2026-03-28 02:35:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/aatagalu/', // 👈 important for GitHub Pages project site
+    base: '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
